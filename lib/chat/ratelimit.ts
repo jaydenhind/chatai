8ed3ac32e92a0e92729d2f798b3b4b026ddcb1be
jaydenhind <74842395+jaydenhind@users.noreply.github.com>
2024-08-17 06:45:42 -0500

    Update ratelimit.ts

--- lib/chat/ratelimit.ts
+++ lib/chat/ratelimit.ts
@@ -1,10 +1,8 @@
 import { Ratelimit } from '@upstash/ratelimit'
-import { kv } from '@vercel/kv'
 import { redirect } from 'next/navigation'
 import { headers } from 'next/headers'
 
 const geminiRatelimit = new Ratelimit({
-  redis: kv,
   limiter: Ratelimit.slidingWindow(60, '1 m'),
   analytics: true,
   prefix: 'gemini_ratelimit'
